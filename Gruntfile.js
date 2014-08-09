c1cb9b5741f4838f0d68d2854b114a4b53f54821
Erjan Gavalji <erjan.gavalji@gmail.com>
2014-08-10 00:23:21 +0300

    Added a multitask, creating child tasks and running them. There might be a better option (temporarily configuring the original tasks and running them, reverting the config after that). Could not make this one. Seems like tasks get called asynchronously after the configuration (configurations set was listed before the tasks started executing). Worth digging.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,8 +16,23 @@
             zeFiles:{
                 expand: true,
                 src: ["**"],
-                cwd: "<%= srcDir %>",
-                dest: distDirs
+                cwd: "<%= srcDir %>/",
+                dest: distDirs,//"<%= distDirBase %>/"
+            }
+        },
+        multiOutput: {
+            initialTasks: {
+                tasks: [
+                    "clean:distDir",
+                    "copy:zeFiles"
+                ],
+                dest: ["<%= distDirBase %>/dist1A", "<%= distDirBase %>/dist2B"]
+            },
+            otherTasks: {
+                tasks : [
+                    "clean:distDir"
+                ],
+                dest: ["./aa/bb", "./cc/dd"]
             }
         }
     });
@@ -25,9 +40,39 @@
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
 
-   grunt.registerTask("default", [
-        "clean:distDir",
-        "copy:zeFiles"
+    grunt.registerMultiTask("multiOutput", "Runs the specified tasks and outputs the results to multiple directories", function(){
+        var destDirs = this.files[0].dest;
+        var subTasks = this.data.tasks;
+
+        var newTaskList = [];
+        var newTaskName = "multiOutput_Child";
+
+        for (var i=0; i<destDirs.length; i++)
+        {
+            var destDir = destDirs[i];
+            for (var k=0; k<subTasks.length; k++)
+            {
+                var subTask = subTasks[k];
+
+                var subTaskSplit = subTask.split(":");
+                var originalSubTaskConfig = grunt.config(subTaskSplit);
+
+                subTaskSplit[1] = subTaskSplit[1] + "_" + i + "_" + k;
+
+                var newTaskConfig = grunt.util._.clone(originalSubTaskConfig);
+                newTaskConfig.dest = destDir;
+                grunt.config(subTaskSplit, newTaskConfig);
+
+                newTaskList.push(subTaskSplit.join(":"));
+            }
+        }
+
+        grunt.registerTask(newTaskName, newTaskList);
+        grunt.task.run(newTaskName);
+    });
+
+    grunt.registerTask("default", [
+        "multiOutput:initialTasks"
     ]);
 
 };
