9d4de1d16913eef94d46addccb98c05da8ba1110
Erjan Gavalji <erjan.gavalji@gmail.com>
2014-08-09 09:45:49 +0300

    [Expectedly failed] Put the basics, a source and multiple destinations.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+﻿module.exports = function (grunt) {
+
+   var distDirs = ["./dist/dist1/", "./dist/dist2/"];
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        srcDir: "./src",
+        distDirBase: "./dist",
+        distDirs: [" <%= distDirBase %>/dist1", "<%= distDirBase %>/dist2"],
+        clean: {
+            distDir: {
+                src: distDirs
+            }
+        },
+        copy: {
+            zeFiles:{
+                expand: true,
+                src: ["**"],
+                cwd: "<%= srcDir %>",
+                dest: distDirs
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+   grunt.registerTask("default", [
+        "clean:distDir",
+        "copy:zeFiles"
+    ]);
+
+};
